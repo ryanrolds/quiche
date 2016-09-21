1d37758216f25cc13050403e816c2b8e8b05bd6a
Osama Yousry <osama.yousry@outlook.com>
2016-09-21 22:39:27 +0200

    Fixed bug with titles that have space replaced with + issue #4 (#6)
    * Fixed bug with titles that have space replaced with + issue #4
    
    * Added pie chart labels feature
    
    * Fixed null value for labels
    
    * Added feature to README
    
    * Added feature to add percentage to pie chart wedges. Issue #5
    
    * Added tests for the added pie chart features
    
    * Added functions to README

--- test/pie.petcentages.test.js
+++ test/pie.petcentages.test.js
@@ -3,13 +3,13 @@ var assert = require('assert');
 
 var quiche = require('../quiche');
 
-describe('Pie Chart percentages', function() {
+describe('Pie Chart', function() {
   var pie = quiche('pie');
   pie.setTransparentBackground();
   pie.addData(3000, 'Foo', 'FF0000');
   pie.addData(2900, 'Bas', '0000FF');
   pie.addData(1500, 'Bar', '00FF00');
-  pie.setLabel(['Foo','Bas','Bar']);
+  pie.addPercent();
   var url = pie.getUrl();
 
   it('should return a url', function() {
@@ -19,7 +19,7 @@ describe('Pie Chart percentages', function() {
   it('should be a pie chart', function() {
     assert.ok(url.indexOf('cht=p') > 0);
   });
-  it('should have labels', function(){
-    assert.ok(url.indexOf('chl=Foo%7CBas%7CBar') !== -1);
+  it('should have percentages', function(){
+    assert.ok(url.indexOf('chl=40.54%25%7C39.19%25%7C20.27%25') !== -1);
   });
 });
